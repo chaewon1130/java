799c4adb3665510a5267f90abb47a70ac84c09e5
DH <chaewon1130@naver.com>
2022-03-02 22:02:40 +0900

    math

--- BaekJoon/src/b_math/B_1712.java
+++ BaekJoon/src/b_math/B_1712.java
@@ -10,17 +10,10 @@ public class B_1712 {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
-		
-		for(int i=1; ;i++) {
-			if(b > c) {
-				System.out.println(-1);
-				break;
-			}
-			if(a+b*i <c*i) {
-				System.out.println(i);
-				break;
-			}
+		if(b>=c) {
+			System.out.println(-1);
+		}else {
+			System.out.println((a/(c-b))+1);
 		}
-		
 	}
 }
